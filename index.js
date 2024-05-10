a66711c24509b147b4923a9b996550dad0ca59f9
LoykDago <loyk.raj@gmail.com>
2024-05-10 15:41:59 +0300

    initial-commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import fragmentShader from "./shaders/fragmentShader.glsl";
 import vertexShader from "./shaders/vertexShader.glsl";
 const scene = new THREE.Scene();
@@ -8,14 +9,13 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-
 const canvas = document.querySelector("#c");
-const renderer = new THREE.WebGLRenderer({ canvas });
+const controls = new OrbitControls(camera, canvas);
+controls.update();
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-const radius = 2;
-const detail = 3;
-const geometry = new THREE.IcosahedronGeometry(radius, detail);
+const geometry = new THREE.SphereGeometry(2, 32, 16);
 const material = new THREE.ShaderMaterial({
   vertexShader: vertexShader,
   fragmentShader: fragmentShader,
@@ -27,8 +27,9 @@ scene.add(ico);
 camera.position.z = 5;
 
 function render() {
-  requestAnimationFrame(render);
   renderer.render(scene, camera);
+  controls.update();
+  requestAnimationFrame(render);
 }
 
 render();
--- shaders/fragmentShader.glsl.js
+++ shaders/fragmentShader.glsl.js
@@ -1,6 +1,9 @@
 export default /* glsl */ `
+varying vec3 vPosition;
+varying vec3 vNormal;
+varying vec2 vUv;
 void main() {
-	vec4 color=vec4( 1.0, 0.2, 1.0, 1.0 );
+	vec4 color=vec4( vec3(step(0.5,vUv.y)), 1.0 );
 	gl_FragColor = color;
 }
 `;
